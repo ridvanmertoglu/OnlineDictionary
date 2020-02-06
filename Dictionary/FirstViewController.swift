9e1b2c5cdddf5bb77c18b293daabd8036cd2752a
ridvanmertoglu <47368549+ridvanmertoglu@users.noreply.github.com>
2020-02-06 15:06:30 +0300

    Initial Commit

---
+++ Dictionary/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Dictionary
+//
+//  Created by RIDVAN on 6.02.2020.
+//  Copyright © 2020 ridvanmertoglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
