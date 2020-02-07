aa7b8469355d5ec1b97919f3fcc57004d5be6b24
ridvanmertoglu <47368549+ridvanmertoglu@users.noreply.github.com>
2020-02-07 22:29:43 +0300

    second commit

--- Dictionary/FirstViewController.swift
+++ Dictionary/FirstViewController.swift
@@ -13,16 +13,15 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
-    
     @IBOutlet weak var definitionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func clickedSearchButton(_ sender: Any) {
-        
-        
+    
         let headers = [
             "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
             "x-rapidapi-key": "bebb0977a7msh7e8c3ff56cf1a9dp16de43jsn2f87071534b1"
@@ -39,23 +38,23 @@ class FirstViewController: UIViewController {
             if (error != nil) {
                 print(error ?? "Error")
             } else {
-                /*let httpResponse = response as? HTTPURLResponse
-                print(httpResponse)*/
-                do {
-                    let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
-                    
-                    DispatchQueue.main.async {
-                        print(jsonResponse["definitions"])
-                        /*if let def = jsonResponse["definitions"] as? Array<Any> {
-                            for element in def {
-                                print(element)
-                            }
-                        }*/
-                    }
-                    
-                } catch {
-                    print("olmadı")
-                }
+                 /*let httpResponse = response as? HTTPURLResponse
+                               print(httpResponse)*/
+                               do {
+                                   let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                                   
+                                   DispatchQueue.main.async {
+                                       print(jsonResponse["definitions"]!)
+                                       /*if let def = jsonResponse["definitions"] as? Array<Any> {
+                                           for element in def {
+                                               print(element)
+                                           }
+                                       }*/
+                                   }
+                                   
+                               } catch {
+                                   print("olmadı")
+                               }
             }
         })
 
